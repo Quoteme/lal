906971224a14713a2d2de560ad3707bc95a17319
quoteme <lucahappel99@gmx.de>
2021-07-04 17:14:12 +0200

    button pressing and left/right ui-components work now

--- app/Main.hs
+++ app/Main.hs
@@ -9,14 +9,14 @@ import Control.Concurrent
 import Control.Exception
 import Control.Monad (when)
 import Data.Bits ((.|.))
-import System.Process (readProcess)
+import System.Process (readProcess, readProcessWithExitCode, spawnProcess)
 import System.Posix.Types (Fd(..))
 
 data UIComponent = UIComponent {
     text  :: IO String
   , color :: IO String -- "red" or "#ff0000"
   , font  :: XftFont
-  , click :: IO ()
+  , click :: Percentage -> Percentage -> IO ()
   }
 
 main :: IO ()
@@ -26,27 +26,45 @@ main = do
   dpy     <- openDisplay ""
   root    <- rootWindow dpy (defaultScreen dpy)
   win     <- makeBar dpy root 15
-  font    <- xftFontOpen dpy (defaultScreenOfDisplay dpy) "scientifica"
+  font    <- xftFontOpen dpy (defaultScreenOfDisplay dpy) "scientifica:size=9:antialias=false"
   selectInput dpy win (exposureMask .|. buttonPressMask)
   setTextProperty dpy win "Hello World" wM_NAME
-  loop dpy win [
-      -- UIComponent (show <$> battery) (return "#ff0000") font (return ())
-      batteryComp font
+  loop dpy win
+    [
+      labelComp font "Bg: "
     , brighnessComp font
-    , UIComponent (return "Hier" ) (return "#00ff00") font (return ())
-    , UIComponent (return "ist"  ) (return "#0000ff") font (return ())
-    , UIComponent (return "Luca" ) (return "#ffffff") font (return ())
+    ]
+    [
+      labelComp font "Bat: "
+    , batteryComp font
+    , spacing font
+    , labelComp font "Ram: "
+    , ramComp font
+    , spacing font
+    , labelComp font "Bg: "
+    , brighnessComp font
+    , spacing font
+    , labelComp font "Vol: "
+    , volumeComp font
+    , spacing font
+    , labelComp font "Heat: "
+    , heatComp font
+    , spacing font
+    , timeComp font
     ]
 
 -- | Main game loop. Update all the windows, poll their requests, ...
-loop :: Display -> Window -> [UIComponent] -> IO ()
-loop dpy win uics = do
+loop :: Display -> Window -> [UIComponent] -> [UIComponent] -> IO ()
+loop dpy win uicsl uicsr = do
   putStrLn "looping"
   thread <- forkIO $ sendExposeEvent dpy win
   mapWindow dpy win
-  draw dpy win uics
-  pollWindow dpy win [] (killThread thread)
-  loop dpy win uics
+  draw dpy win uicsl uicsr
+  pollWindow dpy win uicsl uicsr (do
+      killThread thread
+      draw dpy win uicsl uicsr
+    )
+  loop dpy win uicsl uicsr
 
 -- | Make a simple window
 makeWindow :: Display -> Window -> Position -> Position -> Dimension -> Dimension -> IO Window
@@ -73,13 +91,13 @@ makeBar dpy root height = makeWindow dpy root 0 0 width height
 -- | Poll all the Events the window listens to
 -- The events which are listened to are set by `selectInput ...`
 -- The callback is run after going through the list of events
-pollWindow :: Display -> Window -> [UIComponent] -> IO () -> IO ()
-pollWindow dpy win uics callback = allocaXEvent $ \e -> do
+pollWindow :: Display -> Window -> [UIComponent] -> [UIComponent] -> IO () -> IO ()
+pollWindow dpy win uicsl uicsr callback = allocaXEvent $ \e -> do
   nextEvent dpy e
   ev <- getEvent e
   when (eventName ev == "ButtonPress") (do
     (_,_,_,x,y,_,_,_,_,_) <- get_ButtonEvent e
-    handleClicks dpy uics 0 0
+    handleClicks dpy uicsl uicsr (fromIntegral x) (fromIntegral y)
     )
   sync dpy True
   callback
@@ -88,42 +106,75 @@ pollWindow dpy win uics callback = allocaXEvent $ \e -> do
 -- events
 sendExposeEvent :: Display -> Window -> IO ()
 sendExposeEvent dpy win = do
-  threadDelay (1 * 1000000)
+  threadDelay (3 * 1000000)
   allocaXEvent $ \e -> do
     setEventType e expose
     sendEvent dpy win False noEventMask e
   sync dpy False
 
 -- | Draw on a window
-draw :: Display -> Window -> [UIComponent] -> IO ()
-draw dpy win uics = do
-  gc <- createGC dpy win
-  mapM_ (\(u,i) -> do
-    margin <- uicompSpacing dpy uics i
-    drawUIComp dpy win u margin 10)
-    (zip uics [0..])
-  freeGC dpy gc
+draw :: Display -> Window -> [UIComponent] -> [UIComponent] -> IO ()
+draw dpy win uicsl uicsr = do
+  forEachUIComp dpy uicsl (\dist _ u -> drawUIComp dpy win u dist 10)
+  forEachUIComp dpy (reverse uicsr) (\dist w u -> drawUIComp dpy win u (distanceFromRight dist w) 10)
+  return ()
+    where
+      distanceFromRight dist w = fromIntegral screenWidth - dist - w
+      screenWidth = widthOfScreen (defaultScreenOfDisplay dpy)
 
 -- | Draw text in a window
 drawText:: Display -> Window -> XftFont -> String -> Int -> Int -> String -> IO ()
 drawText dpy win font color x y text = do
   draw <- createDrawable
   withColor color (\c -> xftDrawString draw c font x y text)
+  xftDrawDestroy draw
   where
     colormap = defaultColormap dpy (defaultScreen dpy)
     visual = defaultVisualOfScreen (defaultScreenOfDisplay dpy)
+    createDrawable = xftDrawCreate dpy win visual colormap
     withColor :: String -> (XftColor -> IO a) -> IO a
     withColor = withXftColorName dpy visual colormap
-    createDrawable = xftDrawCreate dpy win visual colormap
 
 drawUIComp :: Display -> Window -> UIComponent -> Int -> Int -> IO ()
 drawUIComp dpy win uic x y = do
   color <- color uic
   text  <- text uic
+  widt <- width dpy uic
+  heig <- height dpy uic
+  clearArea dpy win
+    (fromIntegral x)
+    (fromIntegral (y-8))
+    (fromIntegral widt)
+    (fromIntegral heig)
+    False
   drawText dpy win (font uic) color x y text
 
-handleClicks :: Display -> [UIComponent] -> Int -> Int -> IO ()
-handleClicks dpy uics x y = undefined
+-- | Execute some function for each ui component
+-- Also returns the total width of the text generated
+forEachUIComp :: Display -> [UIComponent] -> (Int -> Int -> UIComponent -> IO ()) -> IO Int
+forEachUIComp dpy uics func = foldl iterator (return margin :: IO Int) uics
+  where
+    iterator d u = do
+      dist <- d
+      widt <- width dpy u
+      func dist widt u
+      return (dist + widt + padding)
+
+handleClicks :: Display -> [UIComponent] -> [UIComponent] -> Int -> Int -> IO ()
+handleClicks dpy uicsl uicsr x y = do
+  forEachUIComp dpy uicsl (activateIfInside x y)
+  w <- forEachUIComp dpy uicsr (\ _ _ _ -> return ())
+  forEachUIComp dpy uicsr (activateIfInside (x-screenWidth+w) y)
+  return ()
+  where
+    screenWidth = fromIntegral $ widthOfScreen (defaultScreenOfDisplay dpy)
+    activateIfInside x y d widt u = do
+      heig <- height dpy u
+      let ins = inside x y d 0 widt heig
+      let px = fromIntegral (x-d)/fromIntegral widt
+      let py = fromIntegral y/fromIntegral heig
+      when ins (click u px py)
+    inside x y dx dy w h = x>=dx && x<=dx+w && y>=dy && y <=dy+h
 
 initColor :: Display -> String -> IO Pixel
 initColor dpy color = do
@@ -144,39 +195,88 @@ slider width val = [ if threshold i then '#' else '·' | i<-[1..width] ]
 wrap :: String -> String -> String -> String
 wrap l r c = l ++ c ++ r
 
-urgencyLevels :: Float -> Float -> Float -> String
-urgencyLevels hight low val
-  | val >= hight  = "#ff0000"
-  | val <= low    = "#00ffff"
-  | otherwise     = "#ffff00"
+padding = 5
+margin  = 10
 
-uicompSpacing :: Display -> [UIComponent] -> Int -> IO Int
-uicompSpacing dpy uics i =
-  (+margin) . sum . map (+padding) . take i
-  <$> sequence [ width dpy u | u<-uics]
-  where
-    padding = 5
-    margin  = 10
+urgencyLevels :: Float -> Float -> Float -> String
+urgencyLevels critical warning val
+  | val >= critical = "darkred"
+  | val >= warning  = "darkorange"
+  | otherwise       = "darkgreen"
 
 width :: Display -> UIComponent -> IO Int
 width dpy uic = do
   text <- text uic
   xglyphinfo_width <$> xftTextExtents dpy (font uic) text
 
+height :: Display -> UIComponent -> IO Int
+height dpy uic = do
+  text <- text uic
+  xglyphinfo_height <$> xftTextExtents dpy (font uic) text
+
 -- Components
+
+spacing font = UIComponent {
+    text  = return "|"
+  , color = return "#646464"
+  , font  = font
+  , click = \px py -> return ()
+  }
+
+labelComp font label = UIComponent {
+    text  = return label
+  , color = return "#646464"
+  , font  = font
+  , click = \px py -> return ()
+  }
 
 batteryComp font = UIComponent {
     text  = show . round . (*100) <$> battery
   , color = return "#eeeeee"
   , font  = font
-  , click = return ()
+  , click = \px py -> return ()
   }
 
 brighnessComp font = UIComponent {
     text  = slider 10 <$> brighness
   , color = return "#eeeeee"
   , font  = font
-  , click = return ()
+  , click = \px py -> do
+    spawnProcess "sudo" ["light", "-S", show . (*100) $ px]
+    return ()
+  }
+
+volumeComp font = UIComponent {
+    text  = slider 10 <$> volume
+  , color = return "#eeeeee"
+  , font  = font
+  , click = \px py -> do
+    spawnProcess "pamixer" ["--set-volume", show . round . (*100) $ px]
+    print "change volume"
+    return ()
+  }
+
+heatComp font = UIComponent {
+    text  = show <$> heat
+  , color = urgencyLevels 75 55 . maximum <$> heat
+  , font  = font
+  , click = \px py -> return ()
+  }
+
+timeComp font = UIComponent {
+    text  = time
+  , color = return "#ababab"
+  , font  = font
+  , click = \px py -> do
+    spawnProcess "xdg-open" ["https://calendar.google.com/"]
+    return ()
+  }
+
+ramComp font = UIComponent {
+    text  = (++"%") . show . round . (*100) <$> ram
+  , color = return "#eeeeee"
+  , font  = font
+  , click = \px py -> return ()
   }
 
 -- Sensors
@@ -197,7 +297,9 @@ volume :: IO Percentage
 volume = parse <$> command
   where
     parse val = (read val :: Float) / 100
-    command = readProcess "pamixer" ["--get-volume"] ""
+    command = do
+      (_,val,_) <- readProcessWithExitCode "pamixer" ["--get-volume"] ""
+      return val
 
 heat :: IO [Celsius]
 heat = parse <$> command
@@ -218,3 +320,6 @@ ram = do
   command <- readProcess "free" [] ""
   let _:total:used:free:_ = (words . (!!1) . lines) command
   return $ (read used :: Percentage) / (read total :: Percentage)
+
+time :: IO String
+time = head . lines <$> readProcess "date" ["+%d.%m.%Y %H:%M"] ""
